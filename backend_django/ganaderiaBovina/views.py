5b2620980b57afe9a9e9d20ebbd63fe77bf612e1
veronicars <vero7nicaxs@uma.es>
2025-04-11 18:13:03 +0200

    Actualización en Corral de "mensaje" de destroy.

--- backend_django/ganaderiaBovina/views.py
+++ backend_django/ganaderiaBovina/views.py
@@ -263,7 +263,7 @@ class CorralViewSet(viewsets.ModelViewSet):
             try:
                 self.perform_destroy(instance)
                 return Response(
-                    {"mensaje":f"El corral {codigo} ha sido eliminado correctamente."},
+                    {"mensaje": f"El corral {codigo} ha sido eliminado correctamente."},
                     status=status.HTTP_200_OK
                 )
             except ProtectedError:
