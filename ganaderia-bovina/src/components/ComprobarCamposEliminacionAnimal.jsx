ff9ad225f9db07a42ff0b4c22433bab5a243fa40
veronicars <vero7nicaxs@uma.es>
2025-04-13 18:38:09 +0200

    Se actualizan comentarios.

--- ganaderia-bovina/src/components/ComprobarCamposEliminacionAnimal.jsx
+++ ganaderia-bovina/src/components/ComprobarCamposEliminacionAnimal.jsx
@@ -1,7 +1,7 @@
 /*
 * ------------------------------------------ ComprobarCamposEliminacionAnimal.jsx: ------------------------------------------
 * Funcionalidad: se emplea para mostrar un mensaje de error y el campo en rojo.
-* Hay mensajes destinados a las vacas, toros o para ambos.
+* Hay mensajes destinados a los animales (vacas/terneros).
 * ERRORES:
 *  - Indicar que el campo es obligatorio.
 * -----------------------------------------------------------------------------------------------------------
@@ -9,12 +9,12 @@
 export const ComprobarCamposEliminacionAnimal = ({ motivo, fechaEliminacion }) => {
     const erroresTemp = {};
 
-    // Validar si se ha seleccionado un motivo
+    // Validaciones comunes para la eliminación de los animales.
     if (!motivo?.trim()) {
         erroresTemp.motivo = "Campo obligatorio";
     }
 
-    // Validar la fecha si el motivo es Muerte o Vendida
+    // Validar la fecha de eliminación si el motivo es Muerte o Vendida
     if ((motivo === "Muerte" || motivo === "Vendida") && !fechaEliminacion?.trim()) {
         erroresTemp.fechaEliminacion = "Campo obligatorio";
     }
