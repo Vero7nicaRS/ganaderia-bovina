b07efd883dc45dd2ff9792332b12b28402c15464
veronicars <vero7nicaxs@uma.es>
2025-04-27 00:56:14 +0200

    Actualización comentarios.

---
+++ ganaderia-bovina/src/pages/02_VacunacionesTratamientos/EliminarVT.jsx
@@ -0,0 +1,238 @@
+/*
+* ------------------------------------------ EliminarVT.jsx: ------------------------------------------
+* Funcionalidad: se muestra un formulario para la eliminación de una vacuna/tratamiento del inventario.
+* Se selecciona el motivo por el cual se desea borrar la vacuna/tratamiento (ERROR o INACTIVA).
+* En caso de que se indique ERROR será eliminado mientras que si se selecciona el motivo INACTIVA,
+* se actualizara el campo "estado".
+* También, se puede escribir un comentario respecto a su eliminación.
+* --------------------------------------------------------------------------------------------------------
+* */
+
+import {NavLink, useLocation, useNavigate} from "react-router-dom";
+import {useContext, useState} from "react";
+import {ComprobarCamposEliminacionToro} from "../../components/ComprobarCamposEliminacionToro.jsx";
+import Swal from "sweetalert2";
+import api from "../../api.js";
+import {VTContext} from "../../DataAnimales/DataVacunasTratamientos/VTContext.jsx";
+export  const EliminarVT = () => {
+
+    //Se utiliza "location" para acceder a los datos (state) que han sido transmitidos mediante el NavLink (modo y animal)
+    const location = useLocation();
+
+    //Hook para navegar
+    const navigate = useNavigate();
+
+    //Se obtiene al animal (toro).
+    const {vt_inventario: estadoInicialVT} = location.state; // Se recupera  animal (toro) desde el state
+    const [vt_inventario] = useState(estadoInicialVT);
+    /* Se obtiene la función eliminarVT para hacer D (eliminar).
+         Para ello se emplea useContext (se accede al contexto) ----> Se utiliza VTContext
+    */
+    const {eliminarVT, actualizarVTEnContexto } = useContext(VTContext);
+    // Estado para almacenar el motivo de eliminación y comentarios
+    const [motivo, setMotivo] = useState("");
+    const [comentarios, setComentarios] = useState("");
+
+    //Se emplea para gestionar el mensaje de error que indica que hay campos obligatorios.
+    const [errores, setErrores] = useState({});
+
+
+    // Manejador para actualizar el motivo seleccionado
+    const handleMotivoChange = (e) => {
+        setMotivo(e.target.value);
+        // Limpiar errores al seleccionar motivo válido
+        setErrores((prevErrores) => ({
+            ...prevErrores,
+            motivo: "", // Se limpia el error.
+        }));
+    };
+
+    // Manejador para actualizar comentarios
+    const handleComentariosChange = (e) => {
+        setComentarios(e.target.value);
+    };
+
+    const validarFormulario = () => {
+        const erroresTemp = ComprobarCamposEliminacionToro({motivo});
+        setErrores(erroresTemp);
+
+        console.log("Errores detectados:", erroresTemp);
+        console.log("¿Formulario válido?", Object.keys(erroresTemp).length === 0);
+
+        return Object.keys(erroresTemp).length === 0; // Retorna true si no hay errores
+    };
+
+    /* ----------------------- MANEJADOR INVENTARIOVTCONTEXT: ELIMINAR -----------------------*/
+// Manejador para eliminar un tratamiento/inventario
+    const handleEliminar = async () => {
+
+        {/*Aparece un mensaje indicando que el usuario no ha seleccionado ningún motivo*/}
+
+        if (!validarFormulario()) return; // Si hay errores, no continúa
+
+        /*
+            Si el inventariovt (vacuna/tratamiento) ha sido eliminado por el motivo de ERROR, se elimina directamente del sistema.
+            Si el inventariovt (vacuna/tratamiento) ha sido eliminado por el motivo de INACTIVA se actualiza el campo de
+            estado.
+        */
+
+        console.log("🧩 ID de la vacuna/tratamiento que se va a eliminar:", vt_inventario.id);
+        /* 1. Se elimina la vacuna/tratamiento del backend pasándole el motivo y comentario en la petición.
+            Y el backend se encarga de:
+                - Motivo ERROR: elimina la vacuna/tratamiento completamente del sistema (base de datos).
+                - Motivo INACTIVA: NO elimina la vacuna/tratamiento del sistema, sino que se actualizan sus datos
+                (campos estado [con el motivo]).
+        */
+        await api.delete(`/inventariovt/${vt_inventario.id}/eliminar/`, {
+            params: {
+                motivo,
+                comentario: comentarios || undefined
+            }
+        });
+
+        /* 2. Una vez que la vacuna/tratamiento ha sido tratado en el backend,
+              ahora hay que ver qué hacer en el frontend.
+                   - Motivo ERROR: se elimina del contexto.
+                   - Motivo INACTIVA: se mantiene en el contexto y aparece con los campos actualizados.
+            */
+        if (motivo === "Error") {
+            /* 2.1 Se elimina SOLAMENTE la vacuna/tratamiento del contexto porque ya ha sido eliminado antes del backend.
+                  Por ello se le pasa el "id" y "false", y "false" significa que
+                  no tiene que volver a hacer la petición al backend.
+            */
+            eliminarVT(vt_inventario.id, false); // Se elimina directamente la vacuna/tratamiento del contexto
+
+            /*Aparece un mensaje indicando que la vacuna/tratamiento ha sido eliminado por un determinado motivo*/
+            Swal.fire({
+                icon: 'success',
+                title: `${vt_inventario.tipo.toLowerCase() === "vacuna" ? "La vacuna" : "El tratamiento"} 
+                    eliminad${vt_inventario.tipo.toLowerCase() === "vacuna" ? "a" : "o"}.`,
+                html: `<strong>${vt_inventario.id}</strong> ha sido eliminado.<br>Motivo: <strong>${motivo}
+                       </strong>${comentarios ? `<br>Comentarios: ${comentarios}` : ""}`,
+                confirmButtonText: 'Aceptar'
+            });
+
+        } else { //Motivo === INACTIVA
+            /* 2.2 Se actualizan los datos de la vacuna/tratamiento del contexto porque
+             ya ha sido eliminado antes del backend. no volver a contactar con el backend, solo contexto
+             Actualización de la vacuna/tratamiento:
+                 - Estado: "Inactiva".
+                 - Comentario: se añade un comentario en caso de que haya introducido información el usuario.
+             */
+            /* Se actualiza el ESTADO de la vacuna/tratamiento a "Inactiva".
+            Además, se añade un comentario en caso de que haya introducido información el usuario*/
+            const vt_inventarioActualizado = {
+                ...vt_inventario,
+                estado: motivo,
+                comentario: comentarios
+            };
+            actualizarVTEnContexto(vt_inventarioActualizado); // Se actualiza el estado del animal en el contexto.
+
+            /* Aparece un mensaje indicando que el animal (toro) ha sido eliminado por un determinado motivo
+                y dado unos comentarios */
+            Swal.fire({
+                icon: 'success',
+                title: `${vt_inventario.tipo.toLowerCase() === "vacuna" ? "La vacuna" : "El tratamiento"} 
+                        eliminad${vt_inventario.tipo.toLowerCase() === "vacuna" ? "a" : "o"}.`,
+                html: `<strong>${vt_inventario.codigo}</strong> ha sido eliminado.
+                       <br>Motivo: <strong>${motivo}</strong> 
+                       </strong>${comentarios ?
+                    `<br>Comentarios: ${comentarios}` : ""}`,
+                confirmButtonText: 'Aceptar'
+            });
+        }
+
+        navigate("/inventario-vt"); // Redirige a la página que contiene el listado de vacunas y/o tratamientos.
+    };
+    /* ----------------------- FIN MANEJADOR INVENTARIOVTCONTEXT: ELIMINAR -----------------------*/
+
+    return (
+        <>
+            <div className="contenedor">
+                <div className="cuadradoEliminar"> ELIMINAR TORO</div>
+                <div className="cuadradoID"> {/* Se muestra el ID del animal (toro) dentro de un cuadrado. */}
+                    <span className="identificador">ID</span>
+                    <input
+                        type="text"
+                        className="cuadro-id"
+                        name="id"
+                        value={vt_inventario.id || ""}
+                        disabled
+                    />
+                </div>
+            </div>
+
+            <hr/>
+
+            <div className="contenedor-flex">
+                <div className="contenedor-izquierda">
+                    <div className="cuadradoMotivoComentario">
+                        Motivo de eliminación
+                    </div>
+
+                    {/* Aparece un checkbox con los diferentes motivos de eliminación:
+                        - ERROR
+                        - INACTIVA
+                    */}
+
+                    <div className="form-check">
+                        <input
+                            className="form-check-input"
+                            type="radio"
+                            name="motivoEliminacion"
+                            id="motivoError"
+                            value="Error"
+                            checked={motivo === "Error"}
+                            onChange={handleMotivoChange}
+                        />
+                        <label className="form-check-label" htmlFor="motivoError">
+                            ERROR
+                        </label>
+                    </div>
+
+                    <div className="form-check">
+                        <input
+                            className="form-check-input"
+                            type="radio"
+                            name="motivoEliminacion"
+                            id="motivoInactiva"
+                            value="Inactiva"
+                            checked={motivo === "Inactiva"}
+                            onChange={handleMotivoChange}
+                        />
+                        <label className="form-check-label" htmlFor="motivoOtros">
+                            OTROS
+                        </label>
+                        {errores.motivo && <div className="mensaje-error">{errores.motivo}</div>}
+                    </div>
+
+                    <div className="mb-3">
+                        <label htmlFor="comentarios" className="cuadradoMotivoComentario">Comentarios</label>
+                        <textarea
+                            className="form-control"
+                            id="comentarios"
+                            rows="3"
+                            value={comentarios}
+                            onChange={handleComentariosChange}
+                        ></textarea>
+                    </div>
+                </div>
+            </div>
+            <>
+                {/* BOTÓN DE CONFIRMAR ELIMINACIÓN (se vuelve a la lista de toros) */}
+                <button type="button"
+                        className="btn btn-info"
+                        onClick={handleEliminar}>
+                    CONFIRMAR ELIMINACIÓN
+                </button>
+
+                {/* BOTÓN DE CANCELAR (se vuelve a la lista de toros) */}
+                <NavLink to="/visualizar-toros" className="btn btn-info">CANCELAR</NavLink>
+            </>
+            {/* BOTÓN DE VOLVER AL MENÚ PRINCIPAL*/}
+            <div className="boton-volver">
+                <NavLink to="/" className="btn btn-info">VOLVER AL MENÚ</NavLink>
+            </div>
+        </>
+    );
+};
