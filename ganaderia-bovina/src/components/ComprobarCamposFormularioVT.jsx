bf916fb16385d24766390759997dc9654af69e05
veronicars <vero7nicaxs@uma.es>
2025-04-26 14:25:48 +0200

    Se comprueba correctamente que el "nombre" de una vacuna/tratamiento debe ser único.

--- ganaderia-bovina/src/components/ComprobarCamposFormularioVT.jsx
+++ ganaderia-bovina/src/components/ComprobarCamposFormularioVT.jsx
@@ -23,18 +23,48 @@ export const ComprobarCamposFormularioVT = (datosVT, tipo, listadoVT) => {
             // erroresTemp.nombre = "El campo nombre es obligatorio";
             erroresTemp.nombre = "Campo obligatorio";
         }else {
-            // Verificar si el nombre ya existe en la lista de vacunas/tratamientos
+            /* Verificar si el nombre ya existe en la lista de vacunas/tratamientos.
+             1. Se comprueba si se está "AGREGANDO" o "MODIFICANDO"
+                - Se comprueba si el "id" es "null" significa que se está "AGREGANDO". En caso contrario,
+                  se está modificando.
+             2.
+                - AGREGAR: Se comprueba si existe alguna vacuna/tratamiento con ese nombre.
+                - MODIFICAR: se realiza un filtrado (filter) para ver cuantas vacunas/tratamientos hay con ese mismo nombre.
+                    Si... ocurren estas dos cosas, significaría que existe otra vacuna/tratamiento en el inventario.
+                    - Hay 2 o más vacunas/tratamientos con ese mismo nombre.
+                    - Si hay 1 vacuna/tratamiento con ese nombre y los "ids" son distintos al que se está modificando.
+           */
+
             const nombreMayuscula = datosVT.nombre.toUpperCase();
+            // Se comprueba si existe alguna vacuna/tratamiento con ese nombre.
             const existeVT = listadoVT.some(v => v.nombre.toUpperCase() === nombreMayuscula);
-            if (existeVT) {
-                erroresTemp.nombre = "Ya existe una vacuna o tratamiento con este nombre";
-                // erroresTemp.nombre = `Ya existe  ${datosVT.tipo === "Vacuna" ? "una vacuna " : "un tratamiento"} con este nombre`
-                //erroresTemp.nombre = `Ya existe una ${datosVT.tipo}con este nombre.`;
+            console.log("Existe vt ", existeVT);
+            if(datosVT.id === null){ // Se está AGREGANDO: "id" es "null"
+
+                if (existeVT) { // Existe una vacuna/tratamiento con ese nombre.
+                    erroresTemp.nombre = `Ya existe  ${datosVT.tipo === "Vacuna" ? "una vacuna " : "un tratamiento"} 
+                                          con este nombre`;
+                }
+            }else{ // Se está MODIFICANDO: "id" NO es "null".
+
+                const vactracduplicados = listadoVT.filter(v => v.nombre.toUpperCase() === nombreMayuscula);
+                /* Si...
+                    - Hay 2 o más vacunas/tratamientos con ese mismo nombre.
+                    - Si hay 1 vacuna/tratamiento con ese nombre y los "ids" son distintos al que se está modificando.
+                * */
+                if (vactracduplicados.length >= 2 || //
+                    (vactracduplicados.length === 1 && vactracduplicados[0].id !== datosVT.id)) {
+                    erroresTemp.nombre = `Ya existe  ${datosVT.tipo === "Vacuna" ? "una vacuna " : "un tratamiento"} 
+                                          con este nombre`;
+                }
             }
         }
 
-        if(!datosVT.unidades?.trim()){
-            erroresTemp.unidades =  "Campo obligatorio";
+        // if(!datosVT.unidades?.trim()){
+        //     erroresTemp.unidades =  "Campo obligatorio";
+        // }
+        if (datosVT.unidades === "" || datosVT.unidades === null || Number(datosVT.unidades) < 0) {
+            erroresTemp.unidades = "Campo obligatorio";
         }
     // Validaciones para agregar una vacuna/tratamiento al animal.
     }else if(tipo === "VTanimal"){
