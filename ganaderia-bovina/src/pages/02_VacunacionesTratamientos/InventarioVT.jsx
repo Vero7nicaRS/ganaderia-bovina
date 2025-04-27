0da8a281a1bf605587b0cfd8dae5a5e055c68b57
veronicars <vero7nicaxs@uma.es>
2025-04-27 04:16:45 +0200

    Se comenta código de eliminar. Se actualiza NavLink para dirigirse a la página "EliminarVT".

--- ganaderia-bovina/src/pages/02_VacunacionesTratamientos/InventarioVT.jsx
+++ ganaderia-bovina/src/pages/02_VacunacionesTratamientos/InventarioVT.jsx
@@ -49,26 +49,27 @@ export const InventarioVT = () => {
     /* ----------------------- MANEJADOR FORMULARIOVTCONTEXT: ELIMINAR -----------------------*/
 
     // Ventana de confirmación de la eliminación de vacunas/tratamiento utilizando SweetAlert2
-    const manejarEliminar = (id, codigo, tipo, nombre) => {
-        Swal.fire({
-            title: `¿Desea eliminar  ${tipo === "Vacuna" ? "la vacuna" : "el tratamiento"} ${codigo} ${nombre} seleccionada?`,
-            text: "¡Esta acción no se puede deshacer!",
-            icon: 'warning',
-            showCancelButton: true,
-            confirmButtonText: 'ELIMINAR',
-            cancelButtonText: 'CANCELAR',
-            reverseButtons: true // Cambia el orden de los botones
-        }).then((result) => {
-            if (result.isConfirmed) {
-                eliminarVT(id);
-                Swal.fire(
-                    'Eliminado!',
-                    `${tipo === "Vacuna" ? "La vacuna" : "El tratamiento"} ha sido eliminad${tipo === "Vacuna" ? "a" : "o"}.`,
-                    'success'
-                );
-            }
-        });
-    };
+    // const manejarEliminar = (id, codigo, tipo, nombre) => {
+    //     Swal.fire({
+    //         title: `¿Desea eliminar  ${tipo === "Vacuna" ? "la vacuna" : "el tratamiento"} ${codigo} ${nombre} seleccionada?`,
+    //         text: "¡Esta acción no se puede deshacer!",
+    //         icon: 'warning',
+    //         showCancelButton: true,
+    //         confirmButtonText: 'ELIMINAR',
+    //         cancelButtonText: 'CANCELAR',
+    //         reverseButtons: true // Cambia el orden de los botones
+    //     }).then((result) => {
+    //         if (result.isConfirmed) {
+    //             eliminarVT(id);
+    //             Swal.fire(
+    //                 'Eliminado!',
+    //                 `${tipo.toLowerCase() === "vacuna" ? "La vacuna" : "El tratamiento"}
+    //                 ha sido eliminad${tipo.toLowerCase() === "vacuna" ? "a" : "o"}.`,
+    //                 'success'
+    //             );
+    //         }
+    //     });
+    // };
 
     return (
 
@@ -170,14 +171,21 @@ export const InventarioVT = () => {
                                             MODIFICAR
                                         </NavLink>
                                         {/* BOTÓN ELIMINAR */}
-                                        <button
+                                        {/*<button*/}
+                                        {/*    className="btn-eliminar"*/}
+                                        {/*     onClick={ () => manejarEliminar(item.id, item.codigo,*/}
+                                        {/*                                           item.tipo, item.nombre)}*/}
+                                        {/*>*/}
+                                        {/*    ELIMINAR*/}
+                                        {/*</button>*/}
+                                        <NavLink
+                                            to="/eliminar-inventario-vt"
+                                            state={{vt_inventario: item}} //Se le pasa el ANIMAL (item).
                                             className="btn-eliminar"
-                                             onClick={ () => manejarEliminar(item.id, item.codigo,
-                                                                                   item.tipo, item.nombre)}
+
                                         >
                                             ELIMINAR
-                                        </button>
-
+                                        </NavLink>
                                     </>
 
                                 </td>
