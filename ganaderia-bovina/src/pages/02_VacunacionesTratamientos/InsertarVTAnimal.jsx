29edbe29c833c3a76e3e0d84e36da0c2300ac5b0
veronicars <vero7nicaxs@uma.es>
2025-04-30 18:09:41 +0200

    Se elimina "InsertarVTAnimal" que no se utiliza

--- ganaderia-bovina/src/pages/02_VacunacionesTratamientos/InsertarVTAnimal.jsx
+++
@@ -1,8 +0,0 @@
-/* No se está usando, ya que se emplea otra nueva carpeta 02_0_Listado_VT_Animales*/
-export const InsertarVTAnimal = () => {
-    return (
-        <div>
-            Insertar vacuna y/o tratamiento a un animal
-        </div>
-    )
-}
