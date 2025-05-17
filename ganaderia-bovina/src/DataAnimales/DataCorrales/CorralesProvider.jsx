0da0faa297c3d2a07a10a2ca620405c0b2fe434f
veronicars <vero7nicaxs@uma.es>
2025-05-17 18:53:12 +0200

    Se eliminan comentarios.

--- ganaderia-bovina/src/DataAnimales/DataCorrales/CorralesProvider.jsx
+++ ganaderia-bovina/src/DataAnimales/DataCorrales/CorralesProvider.jsx
@@ -83,45 +83,6 @@ export const CorralesProvider = ({children}) => {
             console.error("Error al eliminar corral:", error.response?.data || error.message);
         }
     };
-    //setCorrales permite actualizar el estado de corrales (evitando modificar el estado actual "corrales")
-    // const [corrales, setCorrales] = useState(corralesMock);
-    //
-    // const agregarCorral = (nuevoCorral) => {
-    //
-    //     // Cálculo del identificador: es necesario que cada corral añadido tenga un identificador único (ID).
-    //     setCorrales(prevCorrales => {
-    //
-    //         // 1. Se fitra por las corrales existentes
-    //         const idsNumericos = prevCorrales
-    //             .filter(corral => corral.id.startsWith("CORRAL-")) // Solo corrales
-    //             .map(corral => parseInt(corral.id.split("-")[1])) // Se extrae el número del identificador (ID) pasándolo a entero.
-    //             .filter(num => !isNaN(num));
-    //
-    //         // 2. Definir el siguiente ID disponible: se escoge el último Identificador y se le añade 1.
-    //         const siguienteId = idsNumericos.length > 0 ? Math.max(...idsNumericos) + 1 : 1;
-    //
-    //         // 3. Se define cómo va a ser el identificador "CORRAL-num".
-    //         const idUnico = `CORRAL-${siguienteId}`;
-    //
-    //         // 5. Se añade el nuevo corral a la lista de corrales existentes (prevCorrales) y
-    //         // ese corral nuevo (nuevoCorral) que contiene los datos pasados por el argumento y se le indica
-    //         // el identificador único y tipo correspondiente.
-    //         return [...prevCorrales, { ...nuevoCorral, id: idUnico}];
-    //     });
-    // };
-    //
-    // //modificarCorral: Se reemplaza el objeto corral por otro en la lista de corrales.
-    // const modificarCorral = (corralModificado) => {
-    //     setCorrales(
-    //         corrales.map((corral) =>
-    //             corral.id === corralModificado.id ? corralModificado : corral
-    //         )
-    //     );
-    // };
-    //
-    // const eliminarCorral = (id) => {
-    //     setCorrales(corrales.filter((corral) => corral.id !== id));
-    // };
 
     return (
         <CorralesContext.Provider value={{ corrales, agregarCorral, modificarCorral, eliminarCorral }}>
--- ganaderia-bovina/src/DataAnimales/DataVacunasTratamientos/VTProvider.jsx
+++ ganaderia-bovina/src/DataAnimales/DataVacunasTratamientos/VTProvider.jsx
@@ -107,53 +107,6 @@ export const VtProvider = ({children}) => {
             console.error("Error al cargar el listado de vacunas/tratamientos del inventario:", error);
         }
     };
-    // const agregarVT = (nuevoVT) => {
-    //
-    //     if (!nuevoVT.nombre) {
-    //         console.error("El nombre de la vacuna/tratamiento no está definido.");
-    //         return;
-    //     }
-    //     // setVT([...vt, nuevoVT]); No le estoy asignando ningún identificador.
-    //
-    //     // Cálculo del identificador: es necesario que cada vacuna/tratamiento añadido tenga un identificador único
-    //     // que en este caso, es el nombre de la vacuna/tratamiento.
-    //
-    //     // 1. El nombre se convierte a mayúsculas para evitar posibles inconvenientes con las mayúsculas y minúsculas.
-    //     const nombreMayuscula = nuevoVT.nombre.toUpperCase();
-    //     // 2. Se comprueba que el nombre de la vacuna/tratamiento que se desea agregar sea nuevo.
-    //     // Para ello, se comprueba si existe esa vacuna/tratamiento en mayúsculas con las vacunas/tratamientos existentes en mayúscula.
-    //     const existeVT = vt.some(v => v.nombre.toUpperCase() === nombreMayuscula);
-    //
-    //     if(existeVT){
-    //         console.error("Existe una vacuna/tratamiento con este mismo nombre.");
-    //         return;
-    //     }
-    //
-    //     // 1. Se obtiene el último identificador (ID) asignado a las vacunas/tratamientos
-    //     // const siguienteId = vt.length > 0 ? Math.max(...vt.map(v => parseInt(v.id.replace("VT-", "")))) : 0;
-    //     const siguienteId = vt.length > 0 ? Math.max(...vt.map(v => parseInt(v.id.replace("VT-", "")))) + 1 : 1;
-    //
-    //     // 2. Se define el prefijo para una vacuna/tratamiento (VT-)
-    //     const prefijoID = 'VT-';
-    //
-    //     // 3. Se define cómo va a ser el identificador "VT-num"
-    //     const idUnico = `${prefijoID}${siguienteId}`;
-    //
-    //     // 4. Se crea el nuevo tratamiento/vacuna con el identificador único correspondiente.
-    //     setVT([...vt, { ...nuevoVT, id: idUnico}]);
-    // };
-    //
-    // const modificarVT = (vtModificado) => {
-    //     setVT(
-    //         vt.map((vt) =>
-    //             vt.id === vtModificado.id ? vtModificado : vt
-    //         )
-    //     );
-    // };
-    //
-    // const eliminarVT = (id) => {
-    //     setVT(vt.filter((vt) => vt.id !== id));
-    // };
 
     return (
         <VTContext.Provider value={{ vt, agregarVT, modificarVT, eliminarVT,
