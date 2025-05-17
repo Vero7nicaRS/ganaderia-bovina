573b318be1be5a516dd5c68a41aab143084723d5
veronicars <vero7nicaxs@uma.es>
2025-05-18 00:13:05 +0200

    Se añade "nombreUsuario" al Bienvenido.

--- ganaderia-bovina/src/pages/00_Home/Home.jsx
+++ ganaderia-bovina/src/pages/00_Home/Home.jsx
@@ -1,13 +1,15 @@
 import "../../styles/stylesAll.css"
 import {NavBar} from "../../components/NavBar.jsx";
+import {useAuthContext} from "../../authentication/AuthContext.jsx";
 export const Home = () => {
+    const { nombreUsuario } = useAuthContext();
+
     return (
         <div>
-            <div className="cuadradoBienvenida"> ¡Bienvenido, ____ !</div>
+            <div className="cuadradoBienvenida"> ¡Bienvenido, {nombreUsuario} !</div>
             <div className="cuadradoQueHacer"> ¿Qué desea hacer hoy?</div>
             <hr/>
             <NavBar></NavBar>
-            <h2>Bienvenido a Gestión ganadera bovina</h2>
             <p>Esta es la página principal de la aplicación.</p>
 
         </div>
