cde69fdfbacb4abc2e8130ab5ea1f0af83da1539
veronicars <vero7nicaxs@uma.es>
2025-05-18 00:09:02 +0200

    Se indica a qué rutas puede acceder un ADMINISTRADOR.

--- ganaderia-bovina/src/components/NavBar.jsx
+++ ganaderia-bovina/src/components/NavBar.jsx
@@ -1,6 +1,9 @@
 import {NavLink} from "react-router-dom"
+import {useAuthContext} from "../authentication/AuthContext.jsx";
+import "../styles/NavBar.css";
 
 export const NavBar = () => {
+    const { rol, logout } = useAuthContext(); // Controlar lo que ven del NavBar según el rol de usuario.
 
     return (
 
@@ -24,22 +27,25 @@ export const NavBar = () => {
                             </a>
                             <ul className="dropdown-menu">
                                 {/* Enlace a la página "Visualizar animales" */}
-                                <li><NavLink to='/visualizar-animales' className="dropdown-item">Visualizar
-                                    animales</NavLink></li>
-                                <li>
-                                    <hr className="dropdown-divider"/>
+                                <li><NavLink to='/visualizar-animales' className="dropdown-item">
+                                    Visualizar animales</NavLink>
                                 </li>
-                                {/* Enlace a la página "Agregar animal" */}
-                                <li>
-                                    {/*<NavLink to='/agregar-animal' className="dropdown-item">Agregar animal</NavLink>*/}
-
-                                    <NavLink to="/formulario-animal"
-                                             state={{modo: "agregar"}}
-                                             className="dropdown-item">
-                                        Agregar animal
-                                    </NavLink>
-                                </li>
-
+                                {/* Solo el rol de ADMINISTRADOR puede AGREGAR un animal */}
+                                {rol === "Administrador" && (
+                                    <>
+                                        <li>
+                                            <hr className="dropdown-divider"/>
+                                        </li>
+                                        {/* Enlace a la página "Agregar animal" */}
+                                        <li>
+                                            <NavLink to="/formulario-animal"
+                                                     state={{modo: "agregar"}}
+                                                     className="dropdown-item">
+                                                Agregar animal
+                                            </NavLink>
+                                        </li>
+                                    </>
+                                )}
                                 <li>
                                     <hr className="dropdown-divider"/>
                                 </li>
@@ -59,13 +65,27 @@ export const NavBar = () => {
                             </a>
                             <ul className="dropdown-menu">
                                 {/* Enlace a la página "Visualizar animales" */}
-                                <li><NavLink to='/inventario-vt' className="dropdown-item">Inventario</NavLink></li>
                                 <li>
-                                    <hr className="dropdown-divider"/>
+                                    <NavLink to='/inventario-vt' className="dropdown-item">
+                                        Inventario
+                                    </NavLink>
                                 </li>
-                                {/* Enlace a la página "Agregar nuevo tipo" */}
-                                <li><NavLink to='/formulario-vt' state={{modo: "agregar"}} className="dropdown-item">Agregar
-                                    nuevo tipo</NavLink></li>
+                                {/* Solo el rol de ADMINISTRADOR puede AGREGAR un nuevo tipo de vacuna/tratamiento
+                                    al inventario */}
+                                {rol === "Administrador" && (
+                                    <>
+                                        <li>
+                                            <hr className="dropdown-divider"/>
+                                        </li>
+                                        {/* Enlace a la página "Agregar nuevo tipo" */}
+                                        <li>
+                                            <NavLink to='/formulario-vt' state={{modo: "agregar"}}
+                                                     className="dropdown-item">
+                                                Agregar nuevo tipo
+                                            </NavLink>
+                                        </li>
+                                    </>
+                                )}
                                 <li>
                                     <hr className="dropdown-divider"/>
                                 </li>
@@ -74,18 +94,20 @@ export const NavBar = () => {
                                     <NavLink to='/listado-vt-animal' className="dropdown-item">Listado de
                                         animales</NavLink>
                                 </li>
-
-                                <li>
-                                    <hr className="dropdown-divider"/>
-                                </li>
-                                {/* Enlace a la página "Insertar tipo al animal" */}
-                                <li>
-                                    <NavLink to='/formulario-vt-animal' state={{modo: "agregar"}} className="dropdown-item">Insertar tipo al
-                                        animal
-                                    </NavLink>
-                                </li>
-
-
+                                {/* Solo el rol de ADMINISTRADOR puede AGREGAR una vacuna/tratamiento a un animal */}
+                                {rol === "Administrador" && (
+                                    <>
+                                        <li>
+                                            <hr className="dropdown-divider"/>
+                                        </li>
+                                        {/* Enlace a la página "Insertar tipo al animal" */}
+                                        <li>
+                                            <NavLink to='/formulario-vt-animal' state={{modo: "agregar"}}
+                                                     className="dropdown-item">Insertar tipo al animal
+                                            </NavLink>
+                                        </li>
+                                    </>
+                                )}
                             </ul>
 
                         </li>
@@ -97,24 +119,36 @@ export const NavBar = () => {
                             </a>
                             <ul className="dropdown-menu">
                                 {/* Enlace a la página "Lista de corrales" */}
-                                <li><NavLink to='/lista-corrales' className="dropdown-item">Lista de corrales</NavLink>
-                                </li>
                                 <li>
-                                    <hr className="dropdown-divider"/>
+                                    <NavLink to='/lista-corrales' className="dropdown-item">
+                                        Lista de corrales
+                                    </NavLink>
                                 </li>
-                                {/* Enlace a la página "Agregar corral" */}
-                                <li><NavLink to='/formulario-corral' state={{modo: "agregar"}}
-                                             className="dropdown-item">Agregar corral</NavLink>
-                                </li>
+                                {/* Solo el rol de ADMINISTRADOR puede AGREGAR un corral y mover animales del corral*/}
+                                {rol === "Administrador" && (
+                                    <>
+                                        <li>
+                                            <hr className="dropdown-divider"/>
+                                        </li>
+                                        {/* Enlace a la página "Agregar corral" */}
+                                        <li>
+                                            <NavLink to='/formulario-corral' state={{modo: "agregar"}}
+                                                     className="dropdown-item">Agregar corral
+                                            </NavLink>
+                                        </li>
 
-                                <li>
-                                    <hr className="dropdown-divider"/>
-                                </li>
-                                {/* Enlace a la página "Movimiento de corral" */}
-                                <li><NavLink to='/movimiento-de-corral'
-                                             className="dropdown-item">Mover animal de corral</NavLink>
-                                </li>
+                                        <li>
+                                            <hr className="dropdown-divider"/>
+                                        </li>
+                                        {/* Enlace a la página "Movimiento de corral" */}
+                                        <li>
+                                            <NavLink to='/movimiento-de-corral'
+                                                     className="dropdown-item">Mover animal de corral
+                                            </NavLink>
 
+                                        </li>
+                                    </>
+                                )}
                             </ul>
                         </li>
                         <li className="nav-item dropdown">
@@ -125,14 +159,27 @@ export const NavBar = () => {
                             </a>
                             <ul className="dropdown-menu">
                                 {/* Enlace a la página "Lista de inseminaciones" */}
-                                <li><NavLink to='/lista-inseminaciones' className="dropdown-item">Lista de
-                                    inseminaciones</NavLink></li>
                                 <li>
-                                    <hr className="dropdown-divider"/>
+                                    <NavLink to='/lista-inseminaciones' className="dropdown-item">
+                                        Lista de inseminaciones
+                                    </NavLink>
                                 </li>
-                                {/* Enlace a la página "Inseminación animal" */}
-                                <li><NavLink to='/formulario-inseminacion' state={{modo: "agregar"}}
-                                             className="dropdown-item">Inseminación animal</NavLink></li>
+                                {/* Solo el rol de ADMINISTRADOR puede AGREGAR una inseminación a un animal */}
+                                {rol === "Administrador" && (
+                                    <>
+                                        <li>
+                                            <hr className="dropdown-divider"/>
+                                        </li>
+                                        {/* Enlace a la página "Inseminación animal" */}
+                                        <li>
+                                            <NavLink to='/formulario-inseminacion'
+                                                     state={{modo: "agregar"}}
+                                                     className="dropdown-item">
+                                                Inseminación animal
+                                            </NavLink>
+                                        </li>
+                                    </>
+                                )}
                             </ul>
                         </li>
 
@@ -148,17 +195,27 @@ export const NavBar = () => {
                             </a>
                             <ul className="dropdown-menu">
                                 {/* Enlace a la página "Visualizar toros" */}
-                                <li><NavLink to='/visualizar-toros' className="dropdown-item">Visualizar toros</NavLink>
-                                </li>
                                 <li>
-                                    <hr className="dropdown-divider"/>
+                                    <NavLink to='/visualizar-toros' className="dropdown-item">
+                                        Visualizar toros
+                                    </NavLink>
                                 </li>
-                                {/* Enlace a la página "Formulario toro" */}
-                                <li><NavLink to='/formulario-toro' state={{modo: "agregar"}} className="dropdown-item">Agregar
-                                    toro</NavLink></li>
-
+                                {/* Solo el rol de ADMINISTRADOR puede AGREGAR un toro */}
+                                {rol === "Administrador" && (
+                                    <>
+                                        <li>
+                                            <hr className="dropdown-divider"/>
+                                        </li>
+                                        {/* Enlace a la página "Formulario toro" */}
+                                        <li>
+                                            <NavLink to='/formulario-toro' state={{modo: "agregar"}}
+                                                     className="dropdown-item">
+                                                Agregar toro
+                                            </NavLink>
+                                        </li>
+                                    </>
+                                )}
                             </ul>
-
                         </li>
                     </ul>
 
@@ -166,44 +223,30 @@ export const NavBar = () => {
                         {/* Desplegable de la sección "Usuarios" posicionada a la derecha */}
                         <li className="nav-item dropdown">
                             <a className="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown"
-                               aria-expanded="false">Usuarios</a>
+                               aria-expanded="false">Usuario</a>
                             <ul className="dropdown-menu dropdown-menu-end">
                                 {/* Enlace a la página "Perfil de usuario" --> NO IMPLEMENTADO */}
                                 <li>
-                                    <NavLink to='/perfil-usuario' className="dropdown-item">Perfil de usuario</NavLink>
+                                    <NavLink to='/perfil-usuario'
+                                             className="dropdown-item">
+                                        Perfil de usuario
+                                    </NavLink>
                                 </li>
                                 <li>
                                     <hr className="dropdown-divider"/>
                                 </li>
-                                {/* Enlace a la página "Cerrar sesión"  --> NO IMPLEMENTADO */}
-                                <li><NavLink to='/cerrar-sesion' className="dropdown-item">Cerrar sesión</NavLink></li>
+                                {/* Enlace a la página "Cerrar sesión" */}
+                                <li>
+                                    <button onClick={logout}
+                                            className="dropdown-item dropdown-item-button">
+                                        Cerrar sesión
+                                    </button>
+                                </li>
                             </ul>
                         </li>
                     </ul>
-                    {/*<form className="d-flex" role="search">*/}
-                    {/*    <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>*/}
-                    {/*    <button className="btn btn-outline-success" type="submit">Search</button>*/}
-                    {/*</form>*/}
                 </div>
             </div>
         </nav>
-
     )
 }
-
-// <nav className="navbar navbar-expand-lg bg-body-tertiary">
-//     <div className="container-fluid">
-//         <NavLink to='/gestion-vacas' className="navbar-brand" href="#">Gestion vacas </NavLink>
-//         <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-//             <span className="navbar-toggler-icon"></span>
-//         </button>
-//         <div className="collapse navbar-collapse" id="navbarSupportedContent">
-//             <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-//                 <li className="nav-item">
-//                     <NavLink to='/contacto' className="nav-link active">CONTACTO</NavLink>
-//                 </li>
-//             </ul>
-//
-//         </div>
-//     </div>
-// </nav>
