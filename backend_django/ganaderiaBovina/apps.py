ae6cdc2a49b71ba9af2b1aff902b29c34d834918
veronicars <vero7nicaxs@uma.es>
2025-05-18 00:23:03 +0200

    Se añade "def ready" para las señales.

--- backend_django/ganaderiaBovina/apps.py
+++ backend_django/ganaderiaBovina/apps.py
@@ -4,3 +4,6 @@ from django.apps import AppConfig
 class GanaderiabovinaConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'ganaderiaBovina'
+
+    def ready(self):
+        import ganaderiaBovina.signals  # Señales
