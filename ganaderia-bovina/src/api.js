4933cd0724948327a78c12551161b3acbdbc2de2
veronicars <vero7nicaxs@uma.es>
2025-05-18 00:23:38 +0200

    Se añade "api.interceptors.request.use" para el token de autenticación.

--- ganaderia-bovina/src/api.js
+++ ganaderia-bovina/src/api.js
@@ -4,4 +4,14 @@ import axios from 'axios';
 const api = axios.create({
     baseURL: 'http://localhost:8000/api', // URL/Puerto que utiliza BACKEND
 });
+
+
+api.interceptors.request.use((config) => {
+    const token = localStorage.getItem("access");
+    if (token) {
+        config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
+});
+
 export default api;
