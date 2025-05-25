88122d6a485565864f02dd9b578b8f4814950bfe
veronicars <vero7nicaxs@uma.es>
2025-05-26 01:08:10 +0200

    Se añaden comentarios

--- ganaderia-bovina/src/api.js
+++ ganaderia-bovina/src/api.js
@@ -5,7 +5,7 @@ const api = axios.create({
     baseURL: 'http://localhost:8000/api', // URL/Puerto que utiliza BACKEND
 });
 
-
+// Todas las peticiones a la API usan el token una vez que el usuario ha iniciado sesión.
 api.interceptors.request.use((config) => {
     const token = localStorage.getItem("access");
     if (token) {
