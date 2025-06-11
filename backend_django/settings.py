dbb242376f2a1d1986174ed86ddb0b823cfbd516
veronicars <vero7nicaxs@uma.es>
2025-06-12 01:17:07 +0200

    Se añade "decouple" para no mostrar la clave secreta.

--- backend_django/settings.py
+++ backend_django/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 from datetime import timedelta
 from pathlib import Path
-
+from decouple import config
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -20,7 +20,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-57*d_d&0l7t_zk4l)7!b((5-2oxc+emmm1poas@lc@nj&b=8bv'
+SECRET_KEY = config('SECRET_KEY')
+# print("CLAVE SECRETA:", SECRET_KEY)
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
