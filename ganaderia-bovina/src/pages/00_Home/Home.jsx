7ca9ba91c5d002b609332b1707e4a5f94925f251
veronicars <vero7nicaxs@uma.es>
2025-06-14 11:19:32 +0200

    Se añade logo a la página principal.

--- ganaderia-bovina/src/pages/00_Home/Home.jsx
+++ ganaderia-bovina/src/pages/00_Home/Home.jsx
@@ -1,6 +1,8 @@
 import "../../styles/stylesAll.css"
+import "../../styles//Home.css";
 import {NavBar} from "../../components/NavBar.jsx";
 import {useAuthContext} from "../../authentication/AuthContext.jsx";
+import imagen_logo  from "../../assets/ApexGenomic_logo.png"
 export const Home = () => {
     const { nombreUsuario } = useAuthContext();
 
@@ -11,6 +13,8 @@ export const Home = () => {
             <hr/>
             <NavBar></NavBar>
             <p>Esta es la página principal de la aplicación.</p>
+            {/* Aparece el logo de la aplicación */}
+            <img src={imagen_logo} alt="Logo ApexGenomic" className="ApexGenomicLogoHome"/>
 
         </div>
     );
